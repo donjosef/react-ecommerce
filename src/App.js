7438949ef5d80f80296d0c81fd574a69c0b58774
donjoe <giumontanaro@gmail.com>
2019-04-20 19:03:04 +0200

    Import layout in App and use it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout';
+
 import './App.css';
 
 class App extends Component {
     render() {
         return(
             <div className="App">
-            hello there!
+                <Layout></Layout>
             </div>
         )
     }
