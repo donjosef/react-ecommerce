f10c374c5c3eb4b67f146d29ee487499021b1a8c
donjoe <giumontanaro@gmail.com>
2019-04-23 18:07:57 +0200

    Render Landing Page inside App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout';
+import Landing from './components/Landing';
 
 import './App.css';
 
@@ -7,7 +8,9 @@ class App extends Component {
     render() {
         return(
             <div className="App">
-                <Layout></Layout>
+                <Layout>
+                    <Landing />
+                </Layout>
             </div>
         )
     }
