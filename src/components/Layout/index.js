d5fa78fd42fc8a688a5208a717322ebfde10ae83
donjoe <giumontanaro@gmail.com>
2019-04-24 22:59:30 +0200

    Add Form to Layout component so one can see on both landing and products pages

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header';
+import Form from '../Form';
 
 const Layout = (props) => {
   return (
     <div>
       <Header />
       {props.children}
+      <Form />
     </div>
   )
 }
