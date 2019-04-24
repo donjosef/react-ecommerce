54e48a24f3d54c126e3dfee7bf2373762ee04b7d
donjoe <giumontanaro@gmail.com>
2019-04-24 22:31:00 +0200

    Change Products to class
    Because i need componentDidMount to access window and scroll to top

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grid, Button, Typography, Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { products } from '../../products';
@@ -15,56 +15,65 @@ const styles = {
 
 const effects = ['toLeft', 'toRight', 'toTop', 'toBottom'];
 
-const Products = ({ classes }) => {
-    return (
-        <Grid container style={{marginTop: 64}}>
-            {products.map((product, index) => {
-                const random = Math.floor(Math.random() * effects.length);
-                return (
-                    <Grid
-                        item
-                        key={product.id}
-                        xs={12}
-                        sm={6}
-                        md={4}>
-                        <Paper className={classes.paper} square elevation={0}>
-                            <Reveal effect={effects[random]}>
-                                <div style={{
-                                    background: `rgb(
-                                        ${Math.random() * (20 * index)}, ${Math.random() * (20 * index)}, ${Math.random() * (20 * index)}
-                                    )`
-                                }} className="bg-product-transition"></div>
-                            </Reveal>
-                            <Fade left delay={500}>
-                                <div className="product-image">
-                                    <img className="product-image__image" src={product.thumbnail} />
+class Products extends Component {
+
+    componentDidMount() {
+        window.scrollTo(0, 0)
+    }
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <Grid container style={{marginTop: 64}}>
+                {products.map((product, index) => {
+                    const random = Math.floor(Math.random() * effects.length);
+                    return (
+                        <Grid
+                            item
+                            key={product.id}
+                            xs={12}
+                            sm={6}
+                            md={4}>
+                            <Paper className={classes.paper} square elevation={0}>
+                                <Reveal effect={effects[random]}>
+                                    <div style={{
+                                        background: `rgb(
+                                            ${Math.random() * (20 * index)}, ${Math.random() * (20 * index)}, ${Math.random() * (20 * index)}
+                                        )`
+                                    }} className="bg-product-transition"></div>
+                                </Reveal>
+                                <Fade left delay={500}>
+                                    <div className="product-image">
+                                        <img className="product-image__image" src={product.thumbnail} />
+                                    </div>
+                                </Fade>
+                                <div className="product-details">
+                                    <Typography
+                                        color="inherit"
+                                        gutterBottom
+                                        variant="h4">{product.name}
+                                    </Typography>
+                                    <Typography
+                                        color="inherit"
+                                        gutterBottom
+                                        variant="body1">{product.description}
+                                    </Typography>
+                                    <Typography
+                                        color="inherit"
+                                        gutterBottom
+                                        variant="body1">
+                                        Price: <strong>{product.price}</strong>
+                                    </Typography>
+                                    <Button size="medium" color="inherit" variant="outlined">Select</Button>
                                 </div>
-                            </Fade>
-                            <div className="product-details">
-                                <Typography
-                                    color="inherit"
-                                    gutterBottom
-                                    variant="h4">{product.name}
-                                </Typography>
-                                <Typography
-                                    color="inherit"
-                                    gutterBottom
-                                    variant="body1">{product.description}
-                                </Typography>
-                                <Typography
-                                    color="inherit"
-                                    gutterBottom
-                                    variant="body1">
-                                    Price: <strong>{product.price}</strong>
-                                </Typography>
-                                <Button size="medium" color="inherit" variant="outlined">Select</Button>
-                            </div>
-                        </Paper>
-                    </Grid>
-                )
-            })}
-        </Grid>
-    )
+                            </Paper>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        )
+    }   
 }
 
 export default withStyles(styles)(Products);
