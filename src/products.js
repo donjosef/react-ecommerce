ee3e3e1a24a8d57cdbd9daee2f883b18b82ed714
donjoe <giumontanaro@gmail.com>
2019-04-24 12:08:15 +0200

    Create an array of products objects

---
+++ src/products.js
@@ -0,0 +1,99 @@
+import newBlu from './images/new_blu.png';
+import newRed from './images/new_red.png';
+import newBlack from './images/new_black.png';
+import newDark from './images/new_dark.png';
+import newGold from './images/new_gold.png';
+import newIce from './images/new_ice.png';
+import newSky from './images/new_sky.png';
+import newTech from './images/new_tech.png';
+import newTitan from './images/new_titan.png';
+import newUniverse from './images/new_universe.png';
+import newV from './images/new_v.png';
+import newYellow from './images/new_yellow.png';
+
+export const products = [
+    {
+        id: 1,
+        name: 'Watch 1',
+        price: 100,
+        description: 'This is watch 1. Incredible.',
+        thumbnail: newBlu
+    },
+    {
+        id: 2,
+        name: 'Watch 2',
+        price: 95,
+        description: 'This is watch 2. Awesome.',
+        thumbnail: newRed
+    },
+    {
+        id: 3,
+        name: 'Watch 3',
+        price: 122,
+        description: 'This is watch 3. Original.',
+        thumbnail: newBlack
+    },
+    {
+        id: 4,
+        name: 'Watch 4',
+        price: 79,
+        description: 'This is watch 4. Cheap.',
+        thumbnail: newDark
+    },
+    {
+        id: 5,
+        name: 'Watch 5',
+        price: 109,
+        description: 'This is watch 5. Comfortable.',
+        thumbnail: newGold
+    },
+    {
+        id: 6,
+        name: 'Watch 6',
+        price: 99,
+        description: 'This is watch 6. Nice.',
+        thumbnail: newIce
+    },
+    {
+        id: 7,
+        name: 'Watch 7',
+        price: 180,
+        description: 'This is watch 7. Oh yeah!.',
+        thumbnail: newSky
+    },
+    {
+        id: 8,
+        name: 'Watch 8',
+        price: 189,
+        description: 'This is watch 8. Wow.',
+        thumbnail: newTech
+    },
+    {
+        id: 9,
+        name: 'Watch 9',
+        price: 69,
+        description: 'This is watch 9. Masculine.',
+        thumbnail: newTitan
+    },
+    {
+        id: 10,
+        name: 'Watch 10',
+        price: 139,
+        description: 'This is watch 10. Elegant.',
+        thumbnail: newUniverse
+    },
+    {
+        id: 11,
+        name: 'Watch 11',
+        price: 129,
+        description: 'This is watch 11. Trendy.',
+        thumbnail: newV
+    },
+    {
+        id: 12,
+        name: 'Watch 12',
+        price: 99,
+        description: 'This is watch 12. Cool!.',
+        thumbnail: newYellow
+    }    
+];
