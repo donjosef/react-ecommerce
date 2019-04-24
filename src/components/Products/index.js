ec9840a21fb30c2becff39b66b1ea96f6ec7e429
donjoe <giumontanaro@gmail.com>
2019-04-24 21:06:45 +0200

    Refactor Products component with react-reveal for transition effect

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Grid, Button, Typography, Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { products } from '../../products';
+import Reveal from 'react-reveal/Reveal';
+import Fade from 'react-reveal/Fade';
 import './Products.css';
 
 const styles = {
@@ -11,33 +13,58 @@ const styles = {
     }
 };
 
+const effects = ['toLeft', 'toRight', 'toTop', 'toBottom'];
+
 const Products = ({ classes }) => {
     return (
         <Grid container>
-            {products.map(product => (
-                <Grid
-                    item
-                    key={product.id}
-                    xs={12}
-                    sm={6}
-                    md={4}>
-                    <Paper className={classes.paper} square elevation={0}>
-                        <div className="bg-product-transition"></div>
-                        <div className="product-image">
-                            <img className="product-image__image" src={product.thumbnail} />
-                        </div>
-                        <div className="product-details">
-                            <Typography gutterBottom variant="h4">{product.name}</Typography>
-                            <Typography gutterBottom variant="body1">{product.description}</Typography>
-                            <Typography gutterBottom variant="body1">
-                                Price: <strong>{product.price}</strong>
-                            </Typography>
-                        </div>
-                    </Paper>
-                </Grid>
-            ))}
+            {products.map((product, index) => {
+                const random = Math.floor(Math.random() * effects.length);
+                return (
+                    <Grid
+                        item
+                        key={product.id}
+                        xs={12}
+                        sm={6}
+                        md={4}>
+                        <Paper className={classes.paper} square elevation={0}>
+                            <Reveal effect={effects[random]}>
+                                <div style={{
+                                    background: `rgb(
+                                        ${Math.random() * (20 * index)}, ${Math.random() * (20 * index)}, ${Math.random() * (20 * index)}
+                                    )`
+                                }} className="bg-product-transition"></div>
+                            </Reveal>
+                            <Fade left delay={500}>
+                                <div className="product-image">
+                                    <img className="product-image__image" src={product.thumbnail} />
+                                </div>
+                            </Fade>
+                            <div className="product-details">
+                                <Typography
+                                    color="inherit"
+                                    gutterBottom
+                                    variant="h4">{product.name}
+                                </Typography>
+                                <Typography
+                                    color="inherit"
+                                    gutterBottom
+                                    variant="body1">{product.description}
+                                </Typography>
+                                <Typography
+                                    color="inherit"
+                                    gutterBottom
+                                    variant="body1">
+                                    Price: <strong>{product.price}</strong>
+                                </Typography>
+                                <Button size="medium" color="inherit" variant="outlined">Select</Button>
+                            </div>
+                        </Paper>
+                    </Grid>
+                )
+            })}
         </Grid>
     )
 }
-        
+
 export default withStyles(styles)(Products);
