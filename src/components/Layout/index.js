0b61696550caaf915f82ba709b77f06c0d705494
donjoe <giumontanaro@gmail.com>
2019-04-27 21:17:34 +0200

    Remove Form newsletter from Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../Header';
-import Form from '../Form';
 
 const Layout = (props) => {
   return (
     <div>
       <Header />
       {props.children}
-      <Form />
     </div>
   )
 }
