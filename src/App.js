6814264c9ce9efd279dc64a80805ec732e5901f3
donjoe <giumontanaro@gmail.com>
2019-04-27 21:19:53 +0200

    Render Cart component if path of Route is /cart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import Form from './components/Form'
 import Layout from './components/Layout';
 import Landing from './components/Landing';
 import Products from './components/Products';
+import Cart from './components/Cart';
 import { Route } from 'react-router-dom';
 
 import './App.css';
 
 class App extends Component {
+    state = {
+        cart: []
+    }
+
     render() {
-        return(
+        return (
             <div className="App">
                 <Layout>
                     <Route exact path='/' component={Landing} />
                     <Route path='/products' component={Products} />
+                    <Route
+                        path='/cart'
+                        render={() => (
+                            <Cart cart={this.state.cart} />
+                        )}
+                    />
                 </Layout>
+                <Form />
             </div>
         )
     }
