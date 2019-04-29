78c27f9cf3de29809da967540bdf0b27cd579c93
donjoe <giumontanaro@gmail.com>
2019-04-29 14:34:46 +0200

    Add product to cart array. Wrap products component in Context in order to use it

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Landing from './components/Landing';
 import Products from './components/Products';
 import Cart from './components/Cart';
 import { Route } from 'react-router-dom';
+import Context from './context';
 
 import './App.css';
 
@@ -13,21 +14,46 @@ class App extends Component {
         cart: []
     }
 
+    handleAddProduct = (watch) => {
+        const product = this.state.cart.find(product => product.name === watch.name);
+        if(!product) {
+            this.setState(prevState => ({
+                cart: prevState.cart.concat({...watch, quantity: 1})
+            }))
+        } else {
+            this.setState(prevState => ({
+                cart: prevState.cart.map(product => {
+                    return product.name === watch.name 
+                    ? {
+                    ...product, 
+                    quantity: product.quantity + 1 
+                    } 
+                    : product
+                })
+            }))
+        }
+    }
+
     render() {
-       const {location: {pathname}} = this.props;
+        const { location: { pathname } } = this.props;
         return (
             <div className="App">
                 <Layout>
                     <Route exact path='/' component={Landing} />
-                    <Route path='/products' component={Products} />
+                    <Context.Provider value={{ 
+                        cart: this.state.cart,
+                        addProductToCart: this.handleAddProduct 
+                        }}>
+                        <Route path='/products' component={Products} />
+                    </Context.Provider>
                     <Route
                         path='/cart'
                         render={() => (
-                            <Cart cart={this.state.cart} />
+                            <Cart cart={this.state.cart}/>
                         )}
                     />
                 </Layout>
-                {pathname !== '/cart' &&  <Form />}
+                {pathname !== '/cart' && <Form />}
             </div>
         )
     }
