a3908b20734fca1a0e533f48352d503d0d39950b
donjoe <giumontanaro@gmail.com>
2019-04-29 21:47:14 +0200

    Create a webpack config file for development

---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        "historyApiFallback": true
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+        publicPath: "/"
+    }
+});
