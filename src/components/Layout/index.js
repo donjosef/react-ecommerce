05aa5fa354a42fb6a6f2a31c7e7123702ffcdff1
donjoe <giumontanaro@gmail.com>
2019-04-29 14:37:15 +0200

    PAss quantity down to Header component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,7 +4,7 @@ import Header from '../Header';
 const Layout = (props) => {
   return (
     <div>
-      <Header />
+      <Header quantity={props.quantity}/>
       {props.children}
     </div>
   )
