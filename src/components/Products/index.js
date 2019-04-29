5a95cea603af9e767a0b89a17cbd1a03039fd160
donjoe <giumontanaro@gmail.com>
2019-04-29 17:31:15 +0200

    Avoid update of Products

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -9,8 +9,15 @@ class Products extends Component {
         window.scrollTo(0, 0)
     }
 
+    shouldComponentUpdate() {
+        /*
+        every time there is setState inside App (as a consequence of add a product to the cart) a re-render happens. In order to avoid a re-render of Products, and subsequently Product and Dialog, return false.
+        If Products would re-render, Product will generate a new random color, with the consequence of changing the random color of Dialog every time i click on add product to cart.
+        */
+        return false;
+    }
+
     render() {
-        const { classes } = this.props;
         return (
             <Grid container style={{marginTop: '64px'}}>
                 {products.map(product => <Product key={product.id} product={product} /> )}
