55c72c80d7d76d8ccf9668ca61ff5838d9a11136
donjoe <giumontanaro@gmail.com>
2019-07-13 16:14:53 +0200

    Setup redux to inject the store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import { BrowserRouter, Route } from 'react-router-dom';
+import {createStore, applyMiddleware, compose} from 'redux';
+import Authentication from './store/reducers/authentication';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(Authentication, composeEnhancers(applyMiddleware(thunk)));
+
+
+
 import './index.css';
 
 render(
-    <BrowserRouter basename='/react-ecommerce'>
-        <Route component={App}/>
-    </BrowserRouter>, document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter basename='/react-ecommerce'>
+            <Route component={App}/>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
 );
