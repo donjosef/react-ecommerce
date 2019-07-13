d312b08b24a2ad2dbb1ea3658dbf3b4c8c6faf1b
donjoe <giumontanaro@gmail.com>
2019-07-13 18:25:44 +0200

    Define action creators for authentication init, authentication success, and authentication fail

---
+++ src/store/actions/auth.js
@@ -0,0 +1,45 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+const authInit = () => {
+    return {
+        type: actionTypes.AUTH_INIT
+    }
+}
+
+const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        token, 
+        userId
+    }
+}
+
+const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error
+    }
+}
+
+export const auth = (email, password) => {
+    return dispatch => {
+        dispatch(authInit())
+
+        const authData = {
+            email,
+            password,
+            returnSecureToken: true
+        };
+
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDC3DgbMfFj-ygGbJ-MoXoS076_kE4h-8Y', authData)
+        .then(res => {
+            localStorage.setItem('token', res.data.idToken);
+            localStorage.setItem('userId', res.data.localId);
+            dispatch(authSuccess(res.data.idToken, res.data.localId))
+        })
+        .catch(error => {
+            dispatch(authFail(error))
+        })
+    }
+}
